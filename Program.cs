220d4f7d23e5522c1ff2c2e461cb71976e3b164f
Utarn <utarn@yahoo.com>
2020-05-03 18:13:59 +0700

    Starter Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pattern_adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
